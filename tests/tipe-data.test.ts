138835c7c08e1cf5a56aa6678a1873348d332aff
alfattahdicky <alfattahdicky2@gmail.com>
2025-04-29 22:23:36 +0700

    learn type data ts

---
+++ tests/tipe-data.test.ts
@@ -0,0 +1,11 @@
+describe("Data Type", function () {
+  it("should primitive must declare", function () {
+    let name: string = "Dicky AL Fattah";
+    let value: number = 100000;
+    let bool: boolean = true;
+
+    console.info(name);
+    console.info(value);
+    console.info(bool);
+  });
+});
